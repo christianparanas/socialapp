d05c20adf63db393eff8ebc83c14f32cc763211d
Christian Paranas <christiannparanas@gmail.com>
2021-07-27 21:47:53 +0800

    init

---
+++ application/controllers/Auth.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+
+	public function index() 
+	{
+		$this->load->view('Auth/login');
+	}
+
+	public function register()
+	{
+		$this->load->view('Auth/register');
+	}
+}
