029d810ea98eb97fe4f6266d363fcdfdd8629d82
Christian Paranas <christiannparanas@gmail.com>
2021-07-28 20:59:35 +0800

    added some styles

--- application/views/components/nav.php
+++ application/views/components/nav.php
@@ -1,8 +1,18 @@
 <div class="nav_container">
-	<div class="nav_icon_container">
-		<div class="nav_icon"><li class="fad fa-home-alt"></li></div>
-		<div class="nav_icon"><li class="fad fa-user-friends"></i></div>
-		<div class="nav_icon"><li class="fad fa-bell"></i></div>
-		<div class="nav_icon"><li class="fad fa-bars"></li></div>
+	<div class="nav_menu">
+		<div class="nav_icon_container_upper">
+			<div class="nav_icon">codeigniter</div>
+			<div class="nav_icon">
+				<li class="fad fa-search"></i>
+				<li class="fad fa-search"></i>
+			</div>
+		</div>
+
+		<div class="nav_icon_container_lower">
+			<div class="nav_icon"><li class="fad fa-home-alt"></li></div>
+			<div class="nav_icon"><li class="fad fa-user-friends"></i></div>
+			<div class="nav_icon"><li class="fad fa-bell"></i></div>
+			<div class="nav_icon"><li class="fad fa-bars"></li></div>
+		</div>
 	</div>
 </div>
