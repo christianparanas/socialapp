03bb378842f781440775e6903ce35f2ec67ab8ff
Christian Paranas <christiannparanas@gmail.com>
2021-07-29 19:45:12 +0800

    create post btn

--- application/views/Home/create.php
+++ application/views/Home/create.php
@@ -16,7 +16,7 @@
 				</a>
 				<p>Create Post</p>
 			</div>
-			<div class="item2">POST</div>
+			<button class="item2 post_btn" name="post_btn" disabled>POST</button>
 		</div>
 
 		<div class="input_container">
@@ -31,9 +31,23 @@
 					</select>
 				</div>
 			</div>
-			<textarea name="" id="" cols="30" rows="10" placeholder="What's on your mind?"></textarea>
+			<textarea class="text_input" name="" id="" cols="30" rows="10" placeholder="What's on your mind?"></textarea>
 			<hr>
 		</div>
 	</div>
+
+	<script>
+		const textarea_element = document.querySelector('.text_input');
+		const post_btn = document.querySelector('.post_btn');
+
+		textarea_element.addEventListener('input', (event) => {
+		  if(textarea_element.value.length == 0) {
+		  	post_btn.disabled = true;
+		  }
+		  else {
+		  	post_btn.disabled = false;
+		  }
+		});
+	</script>
 </body>
 </html>
