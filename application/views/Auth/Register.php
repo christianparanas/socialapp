c41062a7ef6d0ed79ac6e0bd0ef4d9d2b67c09c9
Christian Paranas <christiannparanas@gmail.com>
2021-07-29 23:38:51 +0800

    make it better

--- application/views/Auth/Register.php
+++ application/views/Auth/Register.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php $this->load->view('components/header'); ?>
-	<link  href="<?= base_url('assets/css/register.css'); ?>" rel="stylesheet" type="text/css">
+	<link  href="<?= base_url('assets/css/Register.css'); ?>" rel="stylesheet" type="text/css">
 	<title>Register</title>
 </head>
 <body>
