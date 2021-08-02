c548d1ae8dc82e88da0c14e18e6a38c60b4e07f9
Christian Paranas <christiannparanas@gmail.com>
2021-08-02 16:18:16 +0800

    deploy

--- application/models/FriendModel.php
+++ application/models/FriendModel.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class FriendModel extends CI_Model {
 
 	public function people_you_may_know() {
-		$this->db->select("id, firstname, lastname");
+		$this->db->select("id, firstname, lastname, profile_pic_url");
 		$this->db->from("users");
 		$this->db->where("id !=", $this->session->id);
 		$this->db->order_by("users.created_at", 'DESC');
